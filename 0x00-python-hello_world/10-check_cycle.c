cc38e50715126ac8d27f7c816f5234dabffb3f41
danielcinome <danielchinome987@gmail.com>
2019-12-09 20:00:13 +0000

    deletd condition

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,16 +13,18 @@ int check_cycle(listint_t *list)
 
 	temp1 = list;
 	temp2 = list->next;
-	if(list == NULL)
-		return (0);
 	while (temp1)
 	{
 		while (temp2)
 		{
 			if (temp1 == temp2)
 				return (1);
+			if (temp2->next == NULL)
+				return (0);
 			temp2 = temp2->next;
 		}
+		if(temp1->next)
+			return(0);
 		temp1 = temp1->next;
 	}
 	return (0);
