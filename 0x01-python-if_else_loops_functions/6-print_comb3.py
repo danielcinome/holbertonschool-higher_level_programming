479ab1c40c0b43c183a01509e8d85a5b0b4bc746
danielcinome <danielchinome987@gmail.com>
2019-12-10 19:19:32 +0000

    correction print

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(10):
-        if i < j and i != 8 and j != 9:
+        if i < j and (i != 8 or j != 9):
             print('{}{}, '.format(i, j), end='')
         elif i == 8 and j == 9:
             print('{}{}'.format(i, j))
-    if i == 9:
-        break
