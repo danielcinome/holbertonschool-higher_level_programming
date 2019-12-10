33b3a0c1a5fcd9524cd401c1b7e21997f92e6d5e
danielcinome <danielchinome987@gmail.com>
2019-12-10 21:06:29 +0000

    correction

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
     for i in range(len(str)):
         car = str[i]
         car = ord(car)
-        if car in range(97, 122):
+        if car in range(97, 123):
             car = car - 32
         print('{:}'.format(chr(car)), end='')
     print('')
