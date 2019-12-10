cbf343430b906e36e0dc9653988a8d59d657f46f
danielcinome <danielchinome987@gmail.com>
2019-12-10 23:20:30 +0000

    chek task

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+* insert_node - check the code for Holberton School students.
+* @head : head
+* @number : number
+* Return: Always 0.
+*/
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *temp;
+	listint_t *new;
+
+	if (head == NULL)
+		return(NULL);
+
+	temp = *head;
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	while (temp != NULL)
+	{
+		if (temp->n > number)
+		{
+			new->next = *head;
+			*head = new;
+			new->n = number;
+		}
+		if (temp->next->n > number)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			new->n = number;
+			break;
+		}
+		temp = temp->next;
+	}
+	return (new);
+}
