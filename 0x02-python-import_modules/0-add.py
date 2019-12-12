d17758dfa861127f02f3f52b4534cbc3d7d4b4ed
danielcinome <danielchinome987@gmail.com>
2019-12-12 12:09:48 -0500

    add codition

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-a = 1
-b = 2
-import add_0 as su
-print('{} + {} = {}'.format(a, b, su.add(int(a), int(b))))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    import add_0 as su
+    print('{} + {} = {}'.format(a, b, su.add(int(a), int(b))))
