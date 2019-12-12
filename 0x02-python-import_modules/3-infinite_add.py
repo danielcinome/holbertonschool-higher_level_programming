b069445248d9db10947a82522aebe53c180fdf54
danielcinome <danielchinome987@gmail.com>
2019-12-12 12:14:21 -0500

    add condtion

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-ran = len(sys.argv)
-res = 0
-if ran == 1:
-    print('{:d}'.format(ran - 1))
-else:
-    for i in range(1, ran):
-        res += int(sys.argv[i])
-    print('{}'.format(res))
+if __name__ == "__main__":
+    import sys
+    ran = len(sys.argv)
+    res = 0
+    if ran == 1:
+        print('{:d}'.format(ran - 1))
+    else:
+        for i in range(1, ran):
+            res += int(sys.argv[i])
+        print('{}'.format(res))
