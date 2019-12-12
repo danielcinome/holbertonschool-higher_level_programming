95ee725fec3edf033c0496d55257681cda576e68
danielcinome <danielchinome987@gmail.com>
2019-12-12 12:07:50 -0500

    correction task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-ran = len(sys.argv)
-if ran == 1:
-    print('{:d} arguments.'.format(ran - 1))
-else:
-    print('{} arguments:'.format(ran - 1))
-    for i in range(1, ran):
-        print('{}: {}'.format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    ran = len(sys.argv)
+    if ran == 1:
+        print('{:d} arguments.'.format(ran - 1))
+    else:
+        print('{} arguments:'.format(ran - 1))
+        for i in range(1, ran):
+            print('{}: {}'.format(i, sys.argv[i]))
