cecd8e42368b55dd309e9a9cf5254b913d6eae2c
danielcinome <danielchinome987@gmail.com>
2019-12-12 12:50:56 -0500

    task complete

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import variable_load_5
-    for i in dir(variable_load_5):
-        if i[0] == "a":
-            print('{}'.format(variable_load_5.a))
+    print('{:d}'.format(variable_load_5.a))
