2afd1661c2913c713635ac3bc0a3fddeb3fd45df
danielcinome <danielchinome987@gmail.com>
2019-12-12 12:12:57 -0500

    add condition

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-a = 10
-b = 5
-import calculator_1 as cal
-print('{} + {} = {}'.format(a, b, cal.add(int(a), int(b))))
-print('{} - {} = {}'.format(a, b, cal.sub(int(a), int(b))))
-print('{} * {} = {}'.format(a, b, cal.mul(int(a), int(b))))
-print('{} / {} = {}'.format(a, b, cal.div(int(a), int(b))))
+if __name__ == "__main__":
+    a = 10
+    b = 5
+    import calculator_1 as cal
+    print('{} + {} = {}'.format(a, b, cal.add(int(a), int(b))))
+    print('{} - {} = {}'.format(a, b, cal.sub(int(a), int(b))))
+    print('{} * {} = {}'.format(a, b, cal.mul(int(a), int(b))))
+    print('{} / {} = {}'.format(a, b, cal.div(int(a), int(b))))
