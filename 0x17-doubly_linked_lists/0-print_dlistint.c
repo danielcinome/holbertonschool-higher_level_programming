5df9f9be7b7684b8a6cb0481b74ba578cb40a451
danielcinome <danielchinome987@gmail.com>
2019-12-13 09:00:03 -0500

    correction task in counter in prev

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,18 +1,37 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - function that prints all the elements of a list
+ * @h : head
+ * Return: number of elements
+ */
+
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t i = 0, a = 0;
 
-	if(h == NULL)
-		return(0);
+	if (h == NULL)
+		return (0);
 
-	while(h != NULL)
+	if (h->next !=  NULL)
 	{
-		a = h->n;
-		printf("%lu\n", a);
-		h = h->next;
-		i++;
+		while (h != NULL)
+		{
+			a = h->n;
+			printf("%lu\n", a);
+			h = h->next;
+			i++;
+		}
 	}
-	return(i);
+	else
+	{
+		while (h != NULL)
+		{
+			a = h->n;
+			printf("%lu\n", a);
+			h = h->prev;
+			i++;
+		}
+	}
+	return (i);
 }
