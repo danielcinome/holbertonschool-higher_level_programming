8dca924f3f428e3cef5f2bafb6f0c5ba504e2758
danielcinome <danielchinome987@gmail.com>
2019-12-17 14:32:31 -0500

    task complete

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for i in range(len(matrix)):
-            print(matrix[i])
-
+            for j in range(len(matrix[i])):
+                print("{}".format(matrix[i][j]), end='')
+                if j < (len(matrix[i]) - 1):
+                    print(" ",end='')
+            print("")
