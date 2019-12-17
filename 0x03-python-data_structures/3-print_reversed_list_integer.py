cd85c12e8184f9a4eb9883a2aa71f86eed9933de
danielcinome <danielchinome987@gmail.com>
2019-12-17 13:06:12 -0500

    addd condition if list is void

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list), 0, -1):
-        a = my_list[i - 1]
-        print("{}".format(a))
+    if my_list:
+        for i in range(len(my_list), 0, -1):
+            a = my_list[i - 1]
+            print("{:d}".format(a))
