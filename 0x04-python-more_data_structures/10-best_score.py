a8fbba0be8ac0298f7bf19e91969726ef300e910
danielcinome <danielchinome987@gmail.com>
2019-12-19 15:56:18 -0500

    task complete

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    val = ''
     if a_dictionary:
-        return max(a_dictionary)
+        val = max(a_dictionary)
+        return val
     else:
         return None
