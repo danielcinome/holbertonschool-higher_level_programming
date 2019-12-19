2345854d70c13caa6e06376acf0c58518e097781
danielcinome <danielchinome987@gmail.com>
2019-12-19 14:09:37 -0500

    correction  print

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     a_dictionary = sorted(a_dictionary.items())
     for i in enumerate(a_dictionary):
-        print(i[1][0], ':', i[1][1])
+        print("{}: {}".format(i[1][0], i[1][1]))
