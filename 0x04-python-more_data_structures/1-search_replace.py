863af2122bcfd114d7cdeaf63390cc3d343d8466
danielcinome <danielchinome987@gmail.com>
2019-12-19 12:05:01 -0500

    deleted condition

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        return list(map(lambda i: replace if i == search else i, my_list))
+    return list(map(lambda i: replace if i == search else i, my_list))
