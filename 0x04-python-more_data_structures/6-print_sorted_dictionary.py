bdcb15242ce99d1ebc416203288ef5ffea4c38fe
danielcinome <danielchinome987@gmail.com>
2019-12-19 14:29:27 -0500

    correction

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     new = list(sorted(a_dictionary.keys()))
     for i in new:
-        print("{}: {}".format(new[1], a_dictionary[i]))
+        print("{}: {}".format(i, a_dictionary[i]))
