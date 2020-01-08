ea20908b06c2167fc8ead2eecbdd8fad774a9e22
danielcinome <danielchinome987@gmail.com>
2020-01-08 13:36:04 -0500

    task complete

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,5 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError or IndexError:
+    except ValueError:
+        return False
+    except TypeError:
         return False
