f4d514586c8a6c0182aa69b113e15d81368aa937
danielcinome <danielchinome987@gmail.com>
2020-01-08 16:34:49 -0500

    task completed

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -7,6 +7,6 @@ def safe_function(fct, *args):
     try:
         res = fct(*args)
         return res
-    except (ValueError, TypeError, ZeroDivisionError, IndexError) as error:
+    except Exception as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return None
