ead22020e948fcf61004c74fbe35cbeb8278125c
danielcinome <danielchinome987@gmail.com>
2020-01-08 16:03:34 -0500

    complted

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    res = 0
+    try:
+        res = fct(args[0], args[1])
+        return res
+    except (ValueError, TypeError, ZeroDivisionError, IndexError) as error:
+        print("Exception: {}".format(error), file=sys.stderr)
