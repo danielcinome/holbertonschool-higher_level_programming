e46cabe9490d63d302fd581958bfc9319d29829c
danielcinome <danielchinome987@gmail.com>
2020-01-08 15:40:19 -0500

    completed

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except (ValueError, TypeError) as ptr_error:
-        print("Exception:{}".format(ptr_error), file=sys.stderr)
+        print("Exception: {}".format(ptr_error), file=sys.stderr)
         return False
