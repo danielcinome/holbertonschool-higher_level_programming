64ad2dcae64a5908fefe5baa1eb0cb41e9e7358e
danielcinome <danielchinome987@gmail.com>
2020-01-09 15:14:42 -0500

    Completed Task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        try:
+            self._size = size
+            if size < 0:
+                raise ValueError
+            if type(size) != int:
+                raise TypeError
+        except TypeError:
+            print("size must be an integer", end='')
+            raise TypeError
+        except ValueError:
+            print("size must be >= 0", end='')
+            raise ValueError
