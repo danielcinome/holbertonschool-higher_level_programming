c1beb4c91e4a96eac38572b44800a16dbca49240
danielcinome <danielchinome987@gmail.com>
2020-01-12 23:26:59 +0000

    Add Documentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,14 +1,14 @@
 #!/bin/usr/python3
-"""1
-   2
-   3
-   4
+"""
+function that prints My name is <first name> <last name>
+first_name and last_name must be strings otherwise, raise a TypeError exception
+with the message first_name must be a string or last_name must be a string
 """
 
 
 def say_my_name(first_name, last_name=""):
-    """1
-       2
+    """
+       prints My name is <first name> <last name>
     """
     if type(first_name) != str:
         raise TypeError("first_name must be a string")
