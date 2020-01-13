4a6354b91828ef70c1b0a5199f81e758cbb842b5
danielcinome <danielchinome987@gmail.com>
2020-01-13 18:34:45 -0500

    f

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+"""
+function that multiplies 2 matrices by using the module NumPy
+Test cases should be the same as 100-matrix_mul
+"""
+import numpy as np
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    multiplies 2 matrices 
+
+    """
+    print()
+    return np.dot(m_a, m_b)
