b5e40e363836e22464ebba45862912bfb3da3919
danielcinome <danielchinome987@gmail.com>
2020-01-13 16:07:04 -0500

    add condition

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -16,9 +16,12 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     for i in range(len(text)):
         if text[i] == ":" or text[i] == "?" or text[i] == "." or a == 1:
-            if a == 1:
-                a = 0
-                continue
+            if a == 1 and text[i] not in (":", ".", "?"):
+                if text[i] == " ":
+                    continue
+                else:
+                    a = 0
+                    print("{}".format(text[i]), end='')
             else:
                 a = 1
                 print("{}".format(text[i]), end='')
