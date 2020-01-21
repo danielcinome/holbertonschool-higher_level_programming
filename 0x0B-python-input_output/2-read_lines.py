adc05156fd56c5e4013228c05d13d2ed86a34f8d
danielcinome <danielchinome987@gmail.com>
2020-01-21 20:51:27 +0000

    Completed task

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+
+def read_lines(filename="", nb_lines=0):
+    with open(filename, 'r') as my_file:
+        n_lines = 0
+        for i in my_file:
+            n_lines += 1
+
+        if nb_lines >= 0 and nb_lines <= n_lines:
+            my_file.seek(0)
+            if nb_lines == 0:
+                print(my_file.read())
+            for j in range(nb_lines):
+                line = my_file.readline()
+                print(line, end='')
