6e0f23e54cf07d892a1d94d07c322388b03fbe0a
danielcinome <danielchinome987@gmail.com>
2020-01-21 23:09:50 +0000

    correction sintax

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,5 +2,5 @@
 
 
 def read_file(filename=""):
-    with open('my_file_0.txt', 'r', encoding='UTF-8') as my_file:
+    with open('my_file_0.txt', 'r', encoding='utf-8') as my_file:
         print(my_file.read())
