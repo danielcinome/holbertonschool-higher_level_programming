97ad6c454eb7d47937d4363e06cc2a1c933abbaa
danielcinome <danielchinome987@gmail.com>
2020-01-22 00:10:23 +0000

    correction add end

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,15 +3,14 @@
 
 def read_lines(filename="", nb_lines=0):
     with open(filename, 'r') as my_file:
-        n_lines = 0
+        n_lines = 1
         for i in my_file:
             n_lines += 1
 
         if nb_lines <= 0 or nb_lines >= n_lines:
             my_file.seek(0)
-            print(my_file.read())
+            print(my_file.read(), end='')
         else:
             my_file.seek(0)
             for j in range(nb_lines):
-                line = my_file.readline()
-                print(line)
+                print(my_file.readline(), end='')
