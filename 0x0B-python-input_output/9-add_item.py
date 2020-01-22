083058fd39dec0409178a02d18f4273e296aae6b
danielcinome <danielchinome987@gmail.com>
2020-01-22 04:41:39 +0000

    ...

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -8,6 +8,6 @@ load = __import__('8-load_from_json_file').load_from_json_file
 try:
     new_file = load("add_item.json")
     new_file += argv[1:]
-except Exception:
+except FileNotFoundError:
     new_file = argv[1:]
 save(new_file, "add_item.json")
