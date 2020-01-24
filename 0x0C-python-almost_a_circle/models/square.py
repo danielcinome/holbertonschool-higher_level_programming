f882c90ae84cdfadc23dc3de0375998bc5e909f1
danielcinome <danielchinome987@gmail.com>
2020-01-24 17:02:39 -0500

    old data

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,7 +5,16 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
+        self.size = size
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        super().validation_type(value, "width")
 
     def __str__(self):
         return str("[Square] ({}) {}/{} - {}/{}".format(
