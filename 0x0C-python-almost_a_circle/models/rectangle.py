125dfcf59056889d987f132bd982bc3bcfe208ec
danielcinome <danielchinome987@gmail.com>
2020-01-24 13:50:25 -0500

    Compplte task 5 main 4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,3 +69,9 @@ class Rectangle(Base):
 
     def area(self):
         return self.__width * self.__height
+
+    def display(self):
+        for i in range(0, self.__height):
+            for j in range(0, self.__width):
+                print("#", end='')
+            print('')
