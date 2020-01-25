14b1698c3336a49a8d664e1f9120226ce518733e
danielcinome <danielchinome987@gmail.com>
2020-01-25 22:30:46 +0000

    Completed task 12 main11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,6 +16,27 @@ class Square(Rectangle):
     def size(self, value):
         self.__size = super().validation_type(value, "width")
 
+    def update(self, *args, **kwargs):
+        if len(args) != 0 and args is not None:
+            if len(args) == 1:
+                self.id = args[0]
+            if len(args) == 2:
+                self.__size = self.validation_type(args[1], "width")
+            if len(args) == 3:
+                self.x = self.validation_type(args[2], "x")
+            if len(args) == 4:
+                self.y = self.validation_x_y(args[3], "y")
+        else:
+            for name, value in kwargs.items():
+                if name == "id":
+                    self.id = value
+                if name == "size":
+                    self.__size = self.validation_type(value, "width")
+                if name == "x":
+                    self.x = self.validation_type(value, "x")
+                if name == "y":
+                    self.y = self.validation_x_y(value, "y")
+
     def __str__(self):
         return str("[Square] ({}) {}/{} - {}".format(
             self.id, self.x, self.y, self.size))
