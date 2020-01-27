038d590aacd934a976275a94e4639796abb14e98
danielcinome <danielchinome987@gmail.com>
2020-01-27 13:24:26 -0500

    add coments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-""" first class Base """
+"""
+first class Base
+"""
 
 
 class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        constructor
+        """
         if id is not None:
             self.id = id
         else:
