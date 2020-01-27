349d2b4dbf849957282fac3c7bdf7273a28838bd
danielcinome <danielchinome987@gmail.com>
2020-01-27 13:42:06 -0500

    Add comments

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-""" class Rectangle that inherits from Base """
+"""
+class Rectangle that inherits from Base
+"""
 from models.base import Base
 
 
 class Rectangle(Base):
+    """
+    Class Rectangle
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Constructor
+        """
+
         self.width = width
         self.height = height
         self.x = x
@@ -35,42 +45,70 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        Getter width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Setter width
+        """
         self.__width = self.validation_type(value, "width")
 
     @property
     def height(self):
+        """
+        Getter height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Setter height
+        """
         self.__height = self.validation_type(value, "height")
 
     @property
     def x(self):
+        """
+        Getter x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        Setter x
+        """
         self.__x = self.validation_x_y(value, "x")
 
     @property
     def y(self):
+        """
+        Getter y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        Setter y
+        """
         self.__y = self.validation_x_y(value, "y")
 
     def area(self):
-        """ Return Area """
+        """
+        Return Area
+        """
         return self.__width * self.__height
 
     def display(self):
-        """ Print Rectangle """
+        """
+        Print Rectangle
+        """
         for k in range(0, self.__y):
             print('')
 
@@ -82,20 +120,26 @@ class Rectangle(Base):
             print('')
 
     def __str__(self):
+        """
+        print in this format
+        """
         return str("[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height))
 
     def update(self, *args, **kwargs):
+        """
+        Update data
+        """
         if len(args) != 0 and args is not None:
-            if len(args) == 1:
+            if len(args) >= 1:
                 super().__init__(args[0])
-            if len(args) == 2:
+            if len(args) >= 2:
                 self.__width = self.validation_type(args[1], "width")
-            if len(args) == 3:
+            if len(args) >= 3:
                 self.__height = self.validation_type(args[2], "height")
-            if len(args) == 4:
+            if len(args) >= 4:
                 self.__x = self.validation_x_y(args[3], "x")
-            if len(args) == 5:
+            if len(args) >= 5:
                 self.__y = self.validation_x_y(args[4], "y")
         else:
             for name, value in kwargs.items():
@@ -111,5 +155,8 @@ class Rectangle(Base):
                     self.__y = self.validation_x_y(value, "y")
 
     def to_dictionary(self):
-        return {'id': self.id, 'widht': self.width, 'height': self.height,
+        """
+        Dictionary
+        """
+        return {'id': self.id, 'width': self.width, 'height': self.height,
                 'x': self.x, 'y': self.y}
