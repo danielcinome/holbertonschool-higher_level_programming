73aa961c064c0b0c81d20053c538f697413afb99
danielcinome <danielchinome987@gmail.com>
2020-03-04 03:14:04 +0000

    [Task 13] Completed

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
 --  database dump from hbtn_0d_tvshows to your MySQL server
 -- (same as 12-no_genre.sql)
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) as number_of_shows FROM tv_genres LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) as number_of_shows FROM tv_show_genres LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
