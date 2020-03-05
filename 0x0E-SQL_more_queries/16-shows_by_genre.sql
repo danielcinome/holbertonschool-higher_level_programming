45542a1a7bd086d42cd0e8ce706c5174cce6dc97
danielcinome <danielchinome987@gmail.com>
2020-03-05 02:36:34 +0000

    [Task 16] Completed

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,3 @@
+-- database dump from hbtn_0d_tvshows to your MySQL server
+-- (same as 15-comedy_only.sql)
+SELECT tv_shows.title, tv_genres.name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id ORDER BY tv_shows.title, tv_genres.name ASC;
