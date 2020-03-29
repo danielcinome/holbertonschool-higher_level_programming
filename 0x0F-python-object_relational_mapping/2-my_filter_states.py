8cb1b0c366c98b5a70508cfdca09d2d8bda68928
danielcinome <danielchinome987@gmail.com>
2020-03-29 03:52:14 +0000

    [Task 2] Correction

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-# Get all states
+"""Get all states"""
 import MySQLdb
 import sys
+
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host='localhost', user=sys.argv[1],
         passwd=sys.argv[2], db=sys.argv[3], port=3306
         )
     cur = db.cursor()
-    cur.execute('SELECT * FROM states WHERE name = (%s)\
-                GROUP BY id ASC', [sys.argv[4]])
+    cur.execute('SELECT * FROM states\
+                GROUP BY id ASC')
     rows = cur.fetchall()
-    for i in range(len(rows)):
-        print(rows[i])
+    for i in rows:
+        if i[1] == sys.argv[4]:
+            print(i)
