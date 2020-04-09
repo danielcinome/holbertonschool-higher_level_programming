7af0e2c4f652d1e64fbc9fb03594114596fa14b2
danielcinome <danielchinome987@gmail.com>
2020-04-09 17:35:46 +0000

    [Task 3] Correction awk

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # cURL only methods
-curl -sI 0.0.0.0:5000/route_4 | grep -i Allow: | awk '{print $2 " " $3 " " $4}'
+curl -sI 0.0.0.0:5000/route_4 | grep -i Allow: | awk -F": " '{print $2}'
