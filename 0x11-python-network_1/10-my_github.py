93d844ed3b5e62669187791969cfb1d90a150b53
danielcinome <danielchinome987@gmail.com>
2020-04-16 19:25:38 +0000

    [Task 10]

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """ My GitHub """
 import requests
-from requests.auth import HTTPBasicAuth
 import sys
 
 
 if __name__ == "__main__":
     user = sys.argv[1]
     token = sys.argv[2]
-    req = requests.get('https://api.github.com/user'
-                       auth=HTTPBasicAuth(user, token))
+    req = requests.get('https://api.github.com/user', auth=(user, token))
     req_dict = req.json()
     print(req_dict.get('id'))
