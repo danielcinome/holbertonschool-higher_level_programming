b85407842468e4cfb517fe55ee871d877e9359d0
danielcinome <danielchinome987@gmail.com>
2020-04-16 19:11:38 +0000

    [Task 8]

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,16 +5,17 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        letter = {'q': sys.argv[1]}
-    else:
-        letter = {'q': ''}
-    req = requests.post("http://0.0.0.0:5000/search_user", data=letter)
-    if (req.json is not None):
-        try:
-            req_dict = req.json()
-            print('[{}] {}'.format(req_dict['id'], req_dict['name']))
-        except ValueError:
-            print('Not a valid JSON')
+    if (len(sys.argv) == 2):
+        if len(sys.argv) > 1:
+            letter = {'q': sys.argv[1]}
+        else:
+            letter = {'q': ''}
+        req = requests.post("http://0.0.0.0:5000/search_user", data=letter)
+        if (req.json is not None):
+            try:
+                req_dict = req.json()
+                print('[{}] {}'.format(req_dict['id'], req_dict['name']))
+            except ValueError:
+                print('Not a valid JSON')
     else:
         print('No result')
