e1c027af07789539ba4b85efa0c12bd0133147c8
danielcinome <danielchinome987@gmail.com>
2020-05-11 18:49:04 +0000

    Task Complted

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w === 0 | h === 0) | (w < 0 | h < 0)) {
+      this.width;
+      this.height;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+module.exports = Rectangle
