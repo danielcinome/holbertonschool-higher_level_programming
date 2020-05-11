19251edc9c2bdfd8d880ba64007e379f479f473e
danielcinome <danielchinome987@gmail.com>
2020-05-11 19:20:56 +0000

    Task Completed

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0 & h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+
+    while (i < this.height) {
+      console.log('X'.repeat(parseInt(this.width, 10)));
+      i++;
+    }
+  }
+}
+module.exports = Rectangle;
