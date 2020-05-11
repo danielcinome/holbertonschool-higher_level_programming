3be0e6535ab4713ac7cf2d9e6bf4929ca6ad0093
danielcinome <danielchinome987@gmail.com>
2020-05-11 19:46:34 +0000

    Task Completed

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,38 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0 & h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+
+    while (i < this.height) {
+      console.log('X'.repeat(parseInt(this.width, 10)));
+      i++;
+    }
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+
+  rotate () {
+    const temp = this.height;
+
+    this.height = this.width;
+    this.width = temp;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+module.exports = Square;
