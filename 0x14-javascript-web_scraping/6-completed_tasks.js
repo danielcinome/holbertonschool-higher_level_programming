4e3e02cbce7e66bdaaca5375ca059cc4cccfc240
danielcinome <danielchinome987@gmail.com>
2020-05-12 20:28:08 +0000

    Task completed

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const argc = process.argv.slice(2);
+const url = argc[0];
+const request = require('request');
+let i = 0;
+let count = 0;
+
+request(url, function (err, response, body) {
+  if (err) {
+    return console.error(err);
+  }
+  const res = JSON.parse(response.body);
+  const obj = {};
+  let act = 1;
+
+  while (i < res.length) {
+    if (act !== res[i].userId) {
+      obj[act] = count;
+      act = res[i].userId;
+      count = 0;
+    }
+    if (act === res[i].userId && res[i].completed) {
+      count++;
+    }
+    i++;
+  }
+  obj[act] = count;
+  console.log(obj);
+});
