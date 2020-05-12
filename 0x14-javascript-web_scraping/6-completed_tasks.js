1cf6a037527a840c3a2d8056f5454a93f37a7bca
danielcinome <danielchinome987@gmail.com>
2020-05-12 21:05:08 +0000

    Check

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,7 +3,6 @@
 const argc = process.argv.slice(2);
 const url = argc[0];
 const request = require('request');
-let count = 0;
 
 request(url, function (err, response, body) {
   if (err) {
@@ -11,18 +10,14 @@ request(url, function (err, response, body) {
   }
   const res = JSON.parse(response.body);
   const obj = {};
-  let act = 1;
-
   res.forEach(element => {
     if (element.completed) {
-      if (act !== element.userId) {
-        obj[act] = count;
-        act = element.userId;
-        count = 0;
+      if (!obj[element.userId]) {
+        obj[element.userId] = 1;
+      } else {
+        obj[element.userId] += 1;
       }
-      count++;
     }
   });
-  obj[act] = count;
   console.log(obj);
 });
